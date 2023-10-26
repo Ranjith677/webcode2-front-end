54b7d19ecf3c47171d4b4503386338a619e78533
Ranjith677 <107233964+Ranjith677@users.noreply.github.com>
2023-10-26 08:02:41 +0530

    1

---
+++ src/components/Login.jsx
@@ -0,0 +1,81 @@
+// import axios from 'axios'
+import axios from 'axios'
+import { useFormik } from 'formik'
+import { useState } from 'react';
+
+import { Link, useNavigate } from 'react-router-dom'
+
+
+function Login() {
+    const[vali,setVali] = useState()
+    let navigate =useNavigate();
+  let formik = useFormik({
+    initialValues: {
+        email: '',
+        password: ''
+    },
+    validate: (values) => {
+        let errors = {}
+        if (values.email === '') {
+            errors.email = "Enter email address"
+        }
+        if(values.password === ''){
+            errors.password ='Enter the password'
+        }
+        return errors
+    },
+    onSubmit:async(values) => {
+        try {
+           let tok = await axios.post('https://webcode2-backend.vercel.app/loginc',values) 
+           if(tok.data.userId){
+            window.localStorage.setItem('app-token',tok.data.token)
+          window.localStorage.setItem('userId',tok.data.userId)
+        
+           
+navigate('/home')
+           }if(tok.data.messege){
+           setVali(tok.data.messege)
+           }
+           console.log(tok)
+        } catch (error) {
+            console.log(error,"its me")
+            
+        }
+    }
+})
+  return (
+    <div className="container">
+    <div className="row justify-content-center">
+        <div className="col-md-6 col-lg-4 col-sm-6  login">
+            <form onSubmit={formik.handleSubmit}>
+                <div class="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" name='email'value={formik.values.email} onChange={formik.handleChange} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <div id="emailHelp" class="form-text">{formik.errors.email ? <div style={{color:'red'}}>Enter valid email </div>:vali ?<div style={{color:'red'}}>{vali} </div> :"Enter registreed email id"}</div>
+                </div>
+                <div class="mb-3">
+                    <label htmlFor="exampleInputPassword1" class="form-label">Password</label>
+                    <input name="password" value={formik.values.password} onChange={formik.handleChange} type="password" class="form-control" id="exampleInputPassword1" />
+               {formik.errors.password ? (<div style={{color:'red'}}>Enter Password</div>) :null
+                
+                
+                }
+                </div>
+                <div>
+                    <Link style={{ marginTop: '32px' }} to={'/forgotpasscode'}>Forgot password</Link>
+                </div>
+                <div className='bton'>
+                    <button type="submit" style={{ backgroundColor: "dodgerblue", border: 'none' }} class="btn btn-primary mt-4">Login</button>
+                </div>
+
+            </form>
+            <div className="signuplnk mt-5">
+                <p>Doesn't have an account?&nbsp;<Link to={'/Signup'}>Signup now</Link></p>
+            </div>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default Login
---
+++ src/stackflow/Questions.jsx
@@ -0,0 +1,78 @@
+import axios from 'axios';
+import { useFormik } from 'formik';
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+function Questions() {
+  let navigate =useNavigate()
+  let formik=useFormik({
+    initialValues:{
+title:'',
+ques:'',
+tags:''
+    },
+    validate:(values)=>{
+let errors ={}
+if(values.title === ''){
+  errors.title ="Enter your name"
+}
+if (values.ques === '') {
+  errors.ques = "Enter email address"
+}
+if(values.tags === ''){
+  errors.tags ='Enter the password'
+}
+return errors
+
+    },
+    onSubmit:async(values)=>{
+try {
+  let postQues=await axios.post('https://webcode2-backend.vercel.app/ques-post',values,{
+    headers:{
+      'auth':window.localStorage.getItem('app-token'),
+      'userid':window.localStorage.getItem('userId')
+    }
+  })
+  console.log(postQues)
+  navigate('/home')
+} catch (error) {
+  console.log(error)
+}
+    }
+  })
+  return (
+    <>
+    <div>Questions</div>
+   <div className="container">
+   <div className="row mt-5 align-self-center align-items-center "><b>Ask your question</b>
+        <div className="col-md-10 mt-5 enterQ">
+    <form onSubmit={formik.handleSubmit}>
+    <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Title</label>
+  <div class="form-text">Be specific and imagine you are asking a question to another person</div>
+  <input type="text" name="title" value={formik.values.title} onChange={formik.handleChange} class="form-control" id="exampleFormControlInput1" placeholder="Enter your title here"/>
+  {formik.errors.title ? <p style={{color:"red"}}>Give a valid title</p>:null}
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Body</label>
+  <div class="form-text">Include all the information someone would need to answer your question</div>
+  <textarea class="form-control" name="ques" value={formik.values.ques} onChange={formik.handleChange} id="exampleFormControlTextarea1" rows="3"></textarea>
+  {formik.errors.ques ? <p style={{color:"red"}}>Explain your question here*</p>:null}
+</div>
+<div class="col-12">
+    <label for="inputAddress2" class="form-label">Tags</label>
+    <div class="form-text">Add up to 5 tags to describe what your question is about</div>
+    
+    <input type="text" name="tags" value={formik.values.tags} onChange={formik.handleChange} class="form-control" id="tags" placeholder="(e.g)c++,javaScript,java,phython"/>
+    {formik.errors.tags ? <p style={{color:"red"}}>Enter some tags to find your question by others</p>:null}
+  </div>
+<button type='submit'  className='btn btn-primary mb-3 mt-4'>Post</button>
+    </form>
+        </div>
+    </div>
+   </div>
+    </>
+  )
+}
+
+export default Questions;
